d6e7f2406da2705b0a497c3e9da4bd8bab6c0c52
hthien <leviethoangthien2005@gmail.com>
2026-03-16 16:38:04 +0700

    fix bugs gây ra các crash của app

--- src/Client/VK.Mobile/Converters/ValueConverters.cs
+++ src/Client/VK.Mobile/Converters/ValueConverters.cs
@@ -149,3 +149,18 @@ public class GreaterThanZeroConverter : IValueConverter
         throw new NotImplementedException();
     }
 }
+
+// Dùng cho: IsVisible khi collection rỗng (count == 0 → true)
+public class IntToBoolInvertedConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is int count) return count == 0;
+        return true;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
